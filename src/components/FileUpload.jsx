08aecd3ea38baa77c1fd219ba3ae739de67fa4b8
CodeByMoin <khanmoin3757@gmail.com>
2025-06-27 16:00:31 +0530

    Fixed Fetch Link

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -45,7 +45,7 @@ export default function FileUpload({ onUploadComplete, darkMode }) {
     formData.append("audio", file);
 
     try {
-      const res = await fetch("http://localhost:5000/api/upload", {
+      const res = await fetch("https://verbatimai-server.onrender.com/api/upload", {
         method: "POST",
         body: formData,
       });
